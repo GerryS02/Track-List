ced8c6f957a8b1f12b488af5e7690abc100aacb3
Gerry Summers <Singur95878@student.edmonds.edu>
2025-03-27 10:03:31 -0700

    modified Song.java

--- song.java
+++ song.java
@@ -1,1 +1,46 @@
 //  file for song details
+
+public class Song{
+    private String title;
+    private String artist;
+    private String genre;
+    private String album;
+    private int year;
+
+    // Constructor
+    public Song(String title, String artist, String genre, String album, int year){
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.album = album;
+        this.year = year;
+    }
+
+    //Getter functions
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    public String getAlbum(){
+        return album;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    // Setters -- to modify the values of these fields after an object has been created.
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+}
