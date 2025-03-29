caff0e16965812f95a77221b1bd2dde63b826577
Gerry Summers <Singur95878@student.edmonds.edu>
2025-03-29 09:37:27 -0700

    updated library features

--- library.java
+++ library.java
@@ -1,1 +1,32 @@
 // file for storing songs
+
+import java.util.*;
+
+public class Library{
+    private Map<String , List<Song>> songsByTitle;
+    private Map<String, List<Song>> songsByArtist;
+    private Map<String, List<Song>> songsByGenre;
+
+    public Library (){
+        songsByTitle = new HashMap<>();
+        songsByArtist = new HashMap<>();
+        songsByGenre = new HashMap<>();
+    }
+
+    // Method to add a song to the library
+
+    public void addSong(Song song){
+        // Add by title
+        songsByTitle.putIfAbsent(song.getTitle(), new ArrayList<>());
+        songsByTitle.get(song.getTitle()).add(song);
+
+        // Add by Artist
+        songsByArtist.putIfAbsent(song.getArtist(), new ArrayList<>());
+        songsByArtist.get(song.getArtist()).add(song);
+
+        // Add by Genre
+        songsByGenre.putIfAbsent(song.getGenre(), new ArrayList<>());
+        songsByGenre.get(song.getGenre()).add(song);
+    }
+
+}
