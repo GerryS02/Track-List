aa5fd5ae6b74eaa02e781aabfeb6718cc8e95641
Gerry Summers <Singur95878@student.edmonds.edu>
2025-04-03 13:41:14 -0700

    added more supporing code

--- main.java
+++ main.java
@@ -1,6 +1,13 @@
 // file for main source code
 // package Track-List;
+import java.util.Scanner;
 
 public class main {
-    
+    public static void main(String[] args){
+        Library collection = new Library();
+        Scanner sc = new Scanner(System.in);
+
+        // Sample songs
+        
+    }
 }
