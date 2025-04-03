61af160aa8edf17975c4277b75611517a30d7bf2
Gerry Summers <Singur95878@student.edmonds.edu>
2025-04-03 13:10:11 -0700

    added more supporting methods

--- library.java
+++ library.java
@@ -29,8 +29,66 @@ public class Library{
         songsByGenre.get(song.getGenre()).add(song);
     }
 
-    // Method to serch a song
+    // Method to serch a songs by title
+    public void searchByTitle(String title){
+        if(songsByTitle.containsKey(title)){
+            // Iterate throgh the list of Songs with that title and display the details
+            for(Song song : songsByTitle.get(title)){
+                song.displaySongDetails();
+            }
+        } else{
+            System.out.println("No songs found with the title: " + title);
+        }
+    }
 
-    
+    // Method to search songs by artist
+
+    public void searchByArtist(String artist){
+        if(songsByArtist.containsKey(artist)){
+            // Iterate through the list of songs by that artist and display the deatils
+            for(Song song : songsByArtist.get(artist)){
+                song.displaySongDetails();
+            }
+        } else {
+            System.out.println("No songs found by "+ artist);
+        }
+    }
+
+    // Method to search by Genre
+
+    public void searchByGenre(String genre){
+        if(songsByGenre.containsKey(genre)){
+            // Iterate through the list of songs associated with that genre and display the details
+            for(Song song : songsByGenre.get(genre)){
+                song.displaySongDetails();
+            }
+        } else {
+            System.out.println("No songs found with the genre: " + genre);
+        }
+    }
+
+    // Method to display all the songs by a specific artist
+    public void listSongsByArtist(String artist){
+        if(songsByArtist.containsKey(artist)){
+            System.out.println("Songs by "+ artist + ":");
+            for(Song song: songsByArtist.get(artist)){
+                // Display the title of the songs
+                System.out.println("- "+ song.getTitle());
+            }
+        } else{
+            System.out.println("No songs found by "+ artist);
+        }
+    }
+
+    //Method to list all the songs by one genre
+    public void listSongsByGenre(String genre){
+        if(songsByGenre.containsKey(genre)){
+            System.out.println("Songs with the "+ genre + " genre: ");
+            for(Song song: songsByGenre.get(genre)){
+                // display only the title
+                System.out.println("- " + song.getTitle());
+            }
+        }
+    }
 
 }
